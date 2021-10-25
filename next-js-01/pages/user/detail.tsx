abe730d6c6ffa0f6557ef42fde0fb6cdd57a1df1
faridrama123 <systemcalls915@gmail.com>
2021-10-25 14:57:58 +0700

    add getstaticprops

--- next-js-01/pages/user/detail.tsx
+++ next-js-01/pages/user/detail.tsx
@@ -1,7 +1,7 @@
 import Layout from "../../component/Layout/layout";
 export default function detail() {
-  return (
-      <>
+  return (  
+    <>
       <Layout pageTitle="Detail User Page">
         <h1>detail user</h1>
       </Layout>
--- next-js-01/pages/user/index.tsx
+++ next-js-01/pages/user/index.tsx
@@ -1,10 +1,31 @@
 import Layout from "../../component/Layout/layout";
-export default function index() {
+
+
+interface UsersProps {
+  dataUsers: Array<any>;
+}
+
+export default function index(props : UsersProps) {
+  const {dataUsers } = props;
+  console.log(dataUsers);
   return (
-      <>
+    <>
       <Layout pageTitle="User Page">
-        <h1>user page</h1>
+        <h1>Data User</h1>
+        {dataUsers.map((user) => (
+          <p key={user.id}> {user.name}</p>
+      ))}
       </Layout>
     </>
   );
+}
+
+export async function getStaticProps (){
+  const res = await fetch ('https://jsonplaceholder.typicode.com/users')
+  const dataUsers = await res.json();
+  return {
+    props :{
+      dataUsers
+    }
+  }
 }
