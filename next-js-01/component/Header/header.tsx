5b2e4b8c35230d03e3aff2cb3d3f62943e6af868
faridrama123 <systemcalls915@gmail.com>
2021-10-26 13:56:35 +0700

    add serverside

--- next-js-01/component/Header/header.tsx
+++ next-js-01/component/Header/header.tsx
@@ -1,24 +1,25 @@
-import Link from 'next/link'
-import styles from './header.module.css'
+import Link from "next/link";
+import styles from "./header.module.css";
 export default function Header() {
   return (
     <>
       <header className={styles.container}>
         <ul className={styles.list}>
           <li className={styles.item}>
-            <Link href="/blog">blog</Link>
+            <Link href="/">Home</Link>
           </li>
           <li className={styles.item}>
-            <Link href="/user">user</Link>
+            <Link href="/blog">Blog</Link>
           </li>
           <li className={styles.item}>
-            <a href="/user/detail">user detail</a>
+            <Link href="/user">User</Link>
           </li>
-             <li className={styles.item}>
+
+          <li className={styles.item}>
             <Link href="/autoredirection">Auto Redirect</Link>
           </li>
         </ul>
       </header>
     </>
-  )
+  );
 }
